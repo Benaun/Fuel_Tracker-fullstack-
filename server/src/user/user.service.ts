21d9327d2da48462bad1fb4c083360c1c6a48d08
A_benaun <Andrey.kuhtin126@gmail.com>
2024-05-14 11:46:00 +0300

    chor: adit user service

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -29,26 +29,11 @@ export class UserService {
 
   async getProfile(id: string) {
     const profile = await this.getById(id);
-    const cityKilometrs = profile.cars?.map((car) =>
-      car.cityDistance.reduce((a: number, b: number) => a + b, 0),
-    );
-    const trackKilometrs = profile.cars?.map((car) =>
-      car.trackDistance.reduce((a: number, b: number) => a + b, 0),
-    );
-    const otherCityKilometrs = profile.cars?.map((car) =>
-      car.otherCityDistance.reduce((a: number, b: number) => a + b, 0),
-    );
-
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { password, ...rest } = profile;
 
     return {
       user: rest,
-      statistics: [
-        { label: 'Город', value: cityKilometrs },
-        { label: 'Трасса', value: trackKilometrs },
-        { label: 'Другой город', value: otherCityKilometrs },
-      ],
     };
   }
 
